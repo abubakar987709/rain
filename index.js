425eca7c469403a399b79b18db8e4a6752c30d32
abubakar987709 <abubakarshafique64@gmail.com>
2025-06-18 20:33:03 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+let a=document.querySelector(".img")
+
+function ab(){
+    a.style.display="block"
+}
+function abc(){
+    a.style.display="none"
+}
+let b=document.querySelector(".img1")
+
+function st(){
+    b.style.display="block"
+}
+function sp(){
+    b.style.display="none"
+}
